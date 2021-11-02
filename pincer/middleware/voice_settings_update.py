95d448bc3fb5b38381b05231c1ca228136ef530e
beastmatser <79206232+beastmatser@users.noreply.github.com>
2021-11-02 20:21:33 +0100

    :sparkles: Added voice_settings_update middleware

--- pincer/middleware/voice_settings_update.py
+++ pincer/middleware/voice_settings_update.py
@@ -3,4 +3,26 @@
 
 """sent when the client's voice settings update"""
 
-# TODO: Implement event
+from ..core.dispatch import GatewayDispatch
+from ..objects.user import VoiceSettings
+from ..utils.conversion import construct_client_dict
+
+
+async def voice_settings_update_middleware(self, payload: GatewayDispatch):
+    """
+    Middleware for ``on_voice_settings_update`` event.
+
+    :param self:
+        The current client.
+
+    :param payload:
+        The data received from the voice settings update event.
+
+    """
+    return "on_voice_settings_update", [
+        VoiceSettings.from_dict(construct_client_dict(self, payload.data))
+    ]
+
+
+def export():
+    return voice_settings_update_middleware
